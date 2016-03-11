778a4bffb0bef4cc214508a8d47fe911fc101697
Arnaud Kervern <akervern@nuxeo.com>
2016-03-11 10:29:17 +0100

    NXP-18966: Use standard AutomationFeature to test Operations

--- generators/operation/classes/test.java
+++ generators/operation/classes/test.java
@@ -1,49 +1,53 @@
 package <%= package %>;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.nuxeo.ecm.automation.client.Session;
-import org.nuxeo.ecm.automation.client.model.Document;
-import org.nuxeo.ecm.automation.test.EmbeddedAutomationServerFeature;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.OperationException;
+import org.nuxeo.ecm.automation.test.AutomationFeature;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
-import org.nuxeo.runtime.test.runner.Jetty;
 
 @RunWith(FeaturesRunner.class)
-@Features(EmbeddedAutomationServerFeature.class)
+@Features(AutomationFeature.class)
 @RepositoryConfig(init = DefaultRepositoryInit.class, cleanup = Granularity.METHOD)
-@Jetty(port = 18080)
 @Deploy("<%= symbolicName %>")
 public class Test<%= s.camelize(operation_name) %> {
 
-      @Inject
-      protected Session clientSession;
+    @Inject
+    protected CoreSession session;
 
-      @Test
-      public void shouldCallTheOperation() throws IOException {
-          Object response = clientSession.newRequest(<%= s.camelize(operation_name) %>.ID).execute();
-          assertTrue(response.getClass().equals(Document.class));
-          Document root = (Document) response;
-          assertEquals("/", root.getPath());
-      }
+    @Inject
+    protected AutomationService automationService;
 
-      @Test
-      public void shouldCallWithParameters() throws IOException {
-          String path = "/default-domain";
-          Object response = clientSession.newRequest(<%= s.camelize(operation_name) %>.ID).set("path", path).execute();
-          assertTrue(response.getClass().equals(Document.class));
-          Document root = (Document) response;
-          assertEquals(path, root.getPath());
-      }
+    @Test
+    public void shouldCallTheOperation() throws OperationException {
+        OperationContext ctx = new OperationContext(session);
+        DocumentModel doc = (DocumentModel) automationService.run(ctx, <%= s.camelize(operation_name) %>.ID);
+        assertEquals("/", doc.getPathAsString());
+    }
+
+    @Test
+    public void shouldCallWithParameters() throws OperationException {
+        final String path = "/default-domain";
+        OperationContext ctx = new OperationContext(session);
+        Map<String, Object> params = new HashMap<>();
+        params.put("path", path);
+        DocumentModel doc = (DocumentModel) automationService.run(ctx, <%= s.camelize(operation_name) %>.ID, params);
+        assertEquals(path, doc.getPathAsString());
+    }
 }
