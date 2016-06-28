204ef3c8290a2b5f828893ce991cd0bfd334a739
Bertrand Chauvin <bchauvin@nuxeo.com>
2016-06-28 18:21:10 +0200

    NXBT-1147 Add document adapter
    NXBT-1147 add unit test for document adapter
    
    NXBT-1147 Use adapter whenever possible in unit test

---
+++ generators/adapter/descriptor.js
@@ -0,0 +1,44 @@
+'use strict';
+var helper = require('../../lib/helpers.js');
+
+module.exports = {
+  depends: 'default',
+  skip: function() {},
+  type: 'core',
+  config: {},
+  params: [{
+    type: 'input',
+    name: 'package',
+    message: 'Document Adapter Package:',
+    store: true,
+    validate: helper.validators.package
+  }, {
+    type: 'input',
+    name: 'doctype',
+    message: 'Document Type ID:',
+    validate: function(value) {
+      var doctypeRegex = '^[a-zA-Z][a-zA-Z0-9_-]*$';
+      return value.match(doctypeRegex) ? true : 'A doctype id should start with a letter, and contain alphanumeric characters, dashes and underscores only.';
+    }
+  }],
+  'main-java': [{
+    src: 'Adapter.java',
+    dest: '{{s.capitalize(doctype)}}Adapter.java'
+  }, {
+    src: 'AdapterFactory.java',
+    dest: '{{s.capitalize(doctype)}}AdapterFactory.java'
+  }],
+  'test-java': [{
+    src: 'TestAdapter.java',
+    dest: 'Test{{s.capitalize(doctype)}}Adapter.java'
+  }],
+  dependencies: [
+    'org.nuxeo.ecm.core:nuxeo-core-api',
+    'org.nuxeo.runtime:nuxeo-runtime',
+    'org.nuxeo.ecm.core:nuxeo-core-test:::test'
+  ],
+  contributions: [{
+    src: 'adapter.xml',
+    dest: '{{s.dasherize(s.decapitalize(package + doctype))}}-adapter-contrib.xml'
+  }]
+};
