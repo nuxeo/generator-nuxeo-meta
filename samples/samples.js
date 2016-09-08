cd8a8753835b14c9eabc3ee9204e57fd7e5a7b12
Arnaud Kervern <akervern@nuxeo.com>
2016-09-08 13:40:58 -0400

    NXBT-1265: Add samples descriptor and test

---
+++ samples/samples.js
@@ -0,0 +1,12 @@
+// Simple array containing all samples available in the nuxeo:sample generator.
+// Format:
+// {
+//   name: 'Title displayed to the end user'
+//   short: 'Title displayed after selection done; shorter is better.'
+//   value: 'github repository',
+// }
+module.exports = [{
+  short: 'Blob Provider',
+  name: 'Blob Provider: contains a sample implementation of the BlobProvider interface with a unit test.',
+  value: 'michaelva/nuxeo-blobprovider-sample'
+}];
---
+++ test/samples.js
@@ -0,0 +1,46 @@
+var seq = require('async/seq');
+var chai = require('chai');
+var dirtyChai = require('dirty-chai');
+chai.use(dirtyChai);
+var expect = chai.expect;
+var path = require('path');
+var request = require('request');
+
+describe('Samples', function() {
+  describe('samples.js', function() {
+
+    before(function(done) {
+      this.samples = require(path.join('..', 'samples', 'samples.js'));
+
+      require('dns').resolve('www.github.com', (errco) => {
+        expect(errco).to.not.exist();
+        done();
+      });
+    });
+
+    it('can be parsed', function() {
+      expect(this.samples).to.be.exist();
+    });
+
+    it('contains only valid repository', function(done) {
+      let validators = [];
+
+      this.samples.forEach((sample) => {
+
+        validators.push((callback) => {
+          request.get(`https://www.github.com/${sample.value}`,
+            function(error, response) {
+              if (!error && response.statusCode !== 200) {
+                error = Error(`Cannot access to ${sample.value}; HTTP status: ${response.statusCode}`);
+              }
+              callback(error);
+            });
+        });
+      });
+
+      seq.apply(this, validators)((error) => {
+        done(error);
+      });
+    });
+  });
+});
