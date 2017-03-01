d43e23b45491d679e045182b7653880ca8374cf3
Arnaud Kervern <akervern@nuxeo.com>
2017-03-01 14:19:09 -0500

    NXBT-1546: Add ReactJS Sample

--- samples/samples.js
+++ samples/samples.js
@@ -9,4 +9,8 @@ module.exports = [{
   short: 'Blob Provider',
   name: 'Blob Provider: contains a sample implementation of the BlobProvider interface with a unit test.',
   value: 'michaelva/nuxeo-blobprovider-sample'
+}, {
+  short: 'ReactJS Application',
+  name: 'ReactJS: contains a sampled ReactJS based application to learn development basics, how to deploy and install it inside Nuxeo Server.',
+  value: 'nuxeo/reactjs-sample-ui'
 }];
