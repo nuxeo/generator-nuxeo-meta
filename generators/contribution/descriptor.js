0c364dec3de32d6adb97b3bf5b4cf5ea78c88fd3
Arnaud Kervern <arnaud.k@gmail.com>
2017-04-21 16:53:39 -0400

    NXBT-1665: Add empty contribution template

---
+++ generators/contribution/descriptor.js
@@ -0,0 +1,33 @@
+var helper = require('../../lib/helpers.js');
+
+module.exports = {
+  params: [{
+    type: 'input',
+    name: 'package',
+    message: 'Contribution package:',
+    store: true,
+    validate: helper.validators.package,
+    filter: function (answer) {
+      return answer.replace(/\s+/g, '.');
+    }
+  }, {
+    type: 'input',
+    name: 'contribution_name',
+    message: 'Contribution name:',
+    validate: helper.validators.only_chars
+  }, {
+    type: 'input',
+    name: 'target',
+    message: 'Target Extension:',
+    validate: helper.validators.package
+  }, {
+    type: 'input',
+    name: 'point',
+    message: 'Extension Point:',
+    validate: helper.validators.only_chars
+  }],
+  contributions: [{
+    src: 'empty.xml',
+    dest: '{{s.dasherize(s.decapitalize(s.titleize(contribution_name)))}}-contrib.xml'
+  }]
+};
