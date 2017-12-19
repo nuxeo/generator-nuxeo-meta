3afc6a4f8bae75ea04b5efe27a3c8113a1907f74
Arnaud Kervern <akervern@nuxeo.com>
2017-12-19 19:08:27 +0100

    NXBT-2061: Add Polymer2 template

---
+++ generators/polymer/descriptor.js
@@ -0,0 +1,67 @@
+var helper = require('../../lib/helpers.js');
+
+module.exports = {
+  depends: 'default',
+  type: 'web',
+  autonomous: true,
+  skip: helper.skips.not_empty_project,
+  params: [{
+    type: 'input',
+    name: 'parent_package',
+    default: 'org.nuxeo.ecm.distribution',
+    message: 'Parent Group id:',
+    store: true,
+    validate: helper.validators.package
+  }, {
+    type: 'input',
+    name: 'parent_artifact',
+    default: 'nuxeo-distribution',
+    message: 'Parent Artifact id:',
+    store: true,
+    validate: helper.validators.artifact
+  }, {
+    type: 'input',
+    name: 'parent_version',
+    message: 'Parent version:',
+    store: true,
+    default: helper.nuxeo_version.default_distribution,
+    validate: helper.validators.version
+  }, {
+    type: 'input',
+    name: 'artifact',
+    message: 'Artifact id:',
+    default: function() {
+      return global._options.dirname + '-web';
+    },
+    validate: helper.validators.artifact
+  }, {
+    type: 'input',
+    name: 'version',
+    message: 'Application Version:',
+    default: '1.0-SNAPSHOT',
+    validate: helper.validators.version_snapshot,
+    when: function(answers) {
+      return !(answers.parent_version && answers.parent_version.match(/-SNAPSHOT$/i));
+    }
+  }, {
+    type: 'input',
+    name: 'name',
+    message: 'Application name:',
+    validate: helper.validators.artifact
+  }, {
+    type: 'input',
+    name: 'route',
+    message: 'Application route:',
+    default: function() {
+      return 'app';
+    },
+    validate: helper.validators.route
+  }],
+  install: [{
+    cmd: 'npm',
+    args: ['install']
+  }, {
+    cmd: 'bower',
+    args: ['install']
+  }]
+};
