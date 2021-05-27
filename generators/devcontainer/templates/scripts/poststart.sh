0d48372b312525dca55d9892ef7d15bcf4bb1638
Gildas Lefevre <glefevre@nuxeo.com>
2021-05-27 17:14:45 +0200

    NXCLI-66: Add uid:gid in docker-compose and fix source path for nuxeo sync

--- generators/devcontainer/templates/scripts/poststart.sh
+++ generators/devcontainer/templates/scripts/poststart.sh
@@ -2,6 +2,6 @@
 # Move to the project folder
 cd ${HOME}/workspace/<%= global._options.dirname %>
 # Configure the synchronize
-nuxeo sync configure --params.deployment="Docker Compose Deployment" --params.serviceName="<%= global._options.dirname %>_nuxeo_1" --params.src="${HOME}/<%= project %>/studio/resources/nuxeo.war/ui" --batch
+nuxeo sync configure --params.deployment="Docker Compose Deployment" --params.serviceName="<%= global._options.dirname %>_nuxeo_1" --params.src="${HOME}/workspace/<%= project %>/studio/resources/nuxeo.war/ui" --batch
 # Configure the hotreload
 nuxeo hotreload configure --params.deployment="Docker Compose Deployment" --params.serviceName="<%= global._options.dirname %>_nuxeo_1" <% if (ignoredModules && ignoredModules.length > 0) { -%> --params.ignoredModules="<%= ignoredModules.join(',') %>" <% } -%> --batch
